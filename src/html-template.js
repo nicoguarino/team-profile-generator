a1b8747a79fc86aa745263378c22d9cd494f473e
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-10-02 19:07:00 -0700

    restructured html-template and wrote psuedo code on where to start next

--- src/html-template.js
+++ src/html-template.js
@@ -1,40 +1,43 @@
 // Here we will create the HTML Template that will pull the function information from the lib folder files
 
 //function that creates cards for properties from object
-//list of employees
+
+const generateEmployeeCard = objectArray =>  {
+
+  //list of employees
+  // use filter and map functions to create cards for each employee. use join method to chain cards
+  return ``;
+
+};
+
 
 
 
 //function that returns a string of good html
-module.exports = templateData => {
-  // destructure page data by section
-  const { } = templateData;
-
-  // create the about section
-
+module.exports =  generateHTML => {
+    
   return `
   <!DOCTYPE html>
   <html lang="en">
- 
+
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Portfolio Demo</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    
     <link rel="stylesheet" href="style.css">
   </head>
 
   <body>
     <header>
-      
+    
     </header>
     <main class="">
-
+      ${generateEmployeeCard()}
     </main>
     <footer class="">
-  
+
     </footer>
   </body>
   </html>
